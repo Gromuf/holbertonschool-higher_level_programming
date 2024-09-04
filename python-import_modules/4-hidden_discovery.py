d89e21f0009b97a2deef342a2fca301e95d82f7f
Gromuf <luigi.beaumois@gmail.com>
2024-09-04 13:56:46 +0200

    i

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,26 +1,20 @@
 #!/usr/bin/python3
-import dis
-import marshal
-import types
+import importlib.util
+import sys
 
 
-def get_names(py_path):
-    with open(py_path, 'rb') as f:
-        f.read(8)
-        code = marshal.load(f)
-    return extract_names(code)
-
-
-def extract_names(code):
-    if isinstance(code, types.CodeType):
-        names = set(code.co_name)
-        for const in code.co_consts:
-            names.update(extract_names(const))
-        return names
-    return set()
+def print_module_names(module):
+    names = sorted(name for name in dir(module) if not name.startswith("__"))
+    for name in names:
+        print(name)
 
 
 if __name__ == "__main__":
-    names = get_names('/tmp/hideen_4.pyc')
-    for name in sorted(name for name in names if not name.startswith("__")):
-        print(name)
+    module_name = "hidden_4"
+    file_path = "/tmp/hidden_4.pyc"
+
+    spec = importlib.util.spec_from_file_location(module_name, file_path)
+    hidden_module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(hidden_module)
+
+    print_module_names(hidden_module)
