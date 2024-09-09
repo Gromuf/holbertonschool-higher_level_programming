d2691ca87a0ee627a48b9e36fa72e20f9078071d
Gromuf <luigi.beaumois@gmail.com>
2024-09-09 10:33:43 +0200

    task 6

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    for i in matrix:
+        for j, element in enumerate(i):
+            if j != 0:
+                print(" ", end="")
+            print("{}".format(element), end="")
+        print()
