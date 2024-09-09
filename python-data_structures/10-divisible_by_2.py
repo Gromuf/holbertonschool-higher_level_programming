f5240203466f652882aef096dbe0cad1e577a564
Gromuf <luigi.beaumois@gmail.com>
2024-09-09 11:25:38 +0200

    task 10

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def divisible_by_2(my_list=[]):
-    new_list = my_list[:]
+    new_list = []
     for i in my_list:
-        new_list[i] = i % 2 == 0
+        new_list.append(i % 2 == 0)
     return new_list
