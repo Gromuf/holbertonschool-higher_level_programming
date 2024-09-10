173597df99b3869c5aed5296e443617e5792f758
Gromuf <luigi.beaumois@gmail.com>
2024-09-10 11:02:56 +0200

    task 0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -3,11 +3,11 @@
 def safe_print_list(my_list=[], x=0):
     result = 0
 
-    try:
-        for i in range(x):
+    for i in range(x):
+        try:
             print("{}".format(my_list[i]), end="")
             result += 1
-    except IndexError:
-        pass
+        except IndexError:
+            break
     print()
     return result
