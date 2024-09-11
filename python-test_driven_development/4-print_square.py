d9da2e1425e18f8ec4b6a3fd9c7aa7933aea233b
Gromuf <luigi.beaumois@gmail.com>
2024-09-11 14:37:44 +0200

    doc

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
 
+"""
+4-print_square
+
+This module provides a function to print a square with the character #.
+"""
+
+
 def print_square(size):
     """
     Prints a square with the character #.
