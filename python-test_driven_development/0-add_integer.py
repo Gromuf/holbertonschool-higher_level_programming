8d199719f2cff5305fe79dd34dfa1bc9f143985f
Gromuf <luigi.beaumois@gmail.com>
2024-09-11 10:44:11 +0200

    add doc

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,6 +1,19 @@
 #!/usr/bin/python3
 
 def add_integer(a, b=98):
+    """
+    Adds two numbers after converting them to integers.
+
+    Parameters:
+    a: The first number (int or float).
+    b: The second number (int or float), defaults to 98.
+
+    Returns:
+    The sum of a and b as an integer.
+
+    Raises:
+    TypeError: If a or b are not integers or floats.
+    """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
