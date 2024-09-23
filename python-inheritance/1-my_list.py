a14afd2dba523399e61a352c026e524eb17d5f72
Gromuf <luigi.beaumois@gmail.com>
2024-09-23 10:17:26 +0200

    task 1

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This module defines a class `MyList` that inherits from the built-in `list`
+class and adds a method to print the list sorted in ascending order.
+"""
+
+
+class MyList(list):
+    """
+    A subclass of `list` with an additional method to print the list sorted.
+    """
+    def print_sorted(self):
+        """
+        Prints the list in ascending order.
+
+        Assumes all elements in the list are integers.
+        """
+        print(sorted(self))
