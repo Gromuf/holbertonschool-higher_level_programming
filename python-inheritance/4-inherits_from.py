08f2a0a6d28db5ef29c19c5eb80d7b982eb257d1
Gromuf <luigi.beaumois@gmail.com>
2024-09-23 14:39:49 +0200

    taask 4

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,22 +1,7 @@
 #!/usr/bin/python3
-"""
-This module defines a function `inherits_from` that checks if an
-object is an instance of a class that inherited (directly or indirectly)
-from the specified class.
-"""
+"""Module pour la fonction inherits_from."""
 
 
 def inherits_from(obj, a_class):
-    """
-    Returns True if the object is an instance of a class that inherited
-    from a_class (directly or indirectly), otherwise returns False.
-
-    Args:
-        obj: The object to check.
-        a_class: The class to check against.
-
-    Returns:
-        bool: True if obj is an instance of a_class or its subclasses,
-              False otherwise.
-    """
-    return isinstance(obj, a_class) and type(obj) is not a_class
+    """Vérifie si obj hérite de a_class."""
+    return issubclass(type(obj), a_class) and type(obj) is not a_class
