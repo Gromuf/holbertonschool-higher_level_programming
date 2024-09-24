60ee85bc9186a313c29b2cc84de4d897ecf3a112
Gromuf <luigi.beaumois@gmail.com>
2024-09-24 10:51:44 +0200

    task 5

--- python-abc/task_05_dragon.py
+++ python-abc/task_05_dragon.py
@@ -11,7 +11,7 @@ class SwimMixin:
 
     def swim(self):
         """Prints a message indicating the creature is swimming."""
-        print("The creature swims")
+        print("The creature swims!")
 
 
 class FlyMixin:
@@ -19,7 +19,7 @@ class FlyMixin:
 
     def fly(self):
         """Prints a message indicating the creature is flying."""
-        print("The creature flies")
+        print("The creature flies!")
 
 
 class Dragon(SwimMixin, FlyMixin):
