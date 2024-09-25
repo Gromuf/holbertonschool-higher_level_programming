8cdfd17a58c94be787d7a6062a4e80c22e7b1f39
Gromuf <luigi.beaumois@gmail.com>
2024-09-25 09:57:12 +0200

    i

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -4,14 +4,34 @@
 
 class MyList(list):
     """
-    A subclass of `list` with an additional method to print the list
+    A subclass of the built-in list that includes a method to print the list
     sorted in ascending order.
+
+    Public Methods:
+        - print_sorted(self): Prints the list sorted in ascending order.
+
+    Example:
+        >>> my_list = MyList()
+        >>> my_list.append(3)
+        >>> my_list.append(1)
+        >>> my_list.append(2)
+        >>> print(my_list)
+        [3, 1, 2]
+        >>> my_list.print_sorted()
+        [1, 2, 3]
+        >>> print(my_list)
+        [3, 1, 2]
     """
 
     def print_sorted(self):
         """
-        Prints the list in ascending order.
+        Prints the list sorted in ascending order.
 
-        Assumes all elements in the list are integers.
+        This method does not modify the original list.
+
+        Example:
+            >>> my_list = MyList([5, 2, 9, 1])
+            >>> my_list.print_sorted()
+            [1, 2, 5, 9]
         """
         print(sorted(self))
