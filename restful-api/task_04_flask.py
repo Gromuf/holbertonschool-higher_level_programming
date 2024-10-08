2212842bcf89b40617687d4ed63cdca6d002ae91
Gromuf <luigi.beaumois@gmail.com>
2024-10-08 14:49:01 +0200

    task 4 without desc

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -1,13 +1,4 @@
 #!/usr/bin/python3
-"""
-A simple Flask API that manages user data.
-
-This API allows you to:
-- View a list of users
-- Check the API status
-- Retrieve details of a specific user
-- Add new users via POST requests
-"""
 
 from flask import Flask, jsonify, request
 
@@ -32,25 +23,21 @@ users = {
 
 @app.route("/")
 def home():
-    """Returns a welcome message for the root endpoint."""
     return "Welcome to the Flask API!"
 
 
 @app.route("/data")
 def username():
-    """Returns a list of all usernames."""
     return jsonify(list(users.keys()))
 
 
 @app.route("/status")
 def status():
-    """Returns the status of the API."""
     return ("OK")
 
 
 @app.route("/users/<username>")
 def get_user(username):
-    """Returns details of a specific user if they exist."""
     user = users.get(username)
     if user:
         return jsonify(user)
@@ -60,7 +47,6 @@ def get_user(username):
 
 @app.route("/add_user", methods=['POST'])
 def add_user():
-    """Handles adding a new user via POST request."""
     new_user = request.get_json()
     if (
         not new_user or 'username' not in new_user
