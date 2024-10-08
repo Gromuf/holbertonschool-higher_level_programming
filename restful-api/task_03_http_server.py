57413ad10b6675a4905808dd5db55c6005e300b1
Gromuf <luigi.beaumois@gmail.com>
2024-10-08 10:45:04 +0200

    task 3

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -48,16 +48,16 @@ class SimpleHTTPServer(http.server.BaseHTTPRequestHandler):
             self.end_headers()
             self.wfile.write(b"OK")
 
-        elif self.path == '/infos':
+        elif self.path == '/info':
             self.send_response(200)
             self.send_header('Content-type', 'application/json')
             self.end_headers()
 
-            infos = {
+            info = {
                 "version": "1.0",
                 "description": "A simple API built with http.server"
             }
-            self.wfile.write(json.dumps(infos).encode())
+            self.wfile.write(json.dumps(info).encode())
 
         else:
             self.send_response(404)
