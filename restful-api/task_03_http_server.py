b6e6456723a0248bd51b4aa8f0f20c0da3d8531a
Gromuf <luigi.beaumois@gmail.com>
2024-10-09 10:30:14 +0200

    task 3

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -39,7 +39,7 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
             self.send_response(200)
             self.send_header('Content-type', 'text/plain')
             self.end_headers()
-            self.wfile.write(b"Hello, this is a simple API!")
+            self.wfile.write("Hello, this is a simple API!".encode('utf-8'))
             logging.info("Réponse envoyée : message de bienvenue.")
 
         elif path == '/data':
@@ -59,7 +59,7 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
             self.send_response(200)
             self.send_header('Content-type', 'text/plain')
             self.end_headers()
-            self.wfile.write(b"OK")
+            self.wfile.write("OK".encode('utf-8'))
             logging.info("Réponse envoyée : statut OK.")
 
         elif path == '/info':
@@ -71,14 +71,14 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
                 "version": "1.0",
                 "description": "A simple API built with http.server"
             }
-            self.wfile.write(json.dumps(info).encode())
+            self.wfile.write(json.dumps(info).encode('utf-8'))
             logging.info("Réponse envoyée : infos.")
 
         else:
             self.send_response(404)
             self.send_header('Content-type', 'text/plain')
             self.end_headers()
-            self.wfile.write(b"Error: Endpoint not found")
+            self.wfile.write("Error: Endpoint not found".encode('utf-8'))
             logging.warning("Réponse envoyée : Endpoint not found")
 
     def log_message(self, format, *args):
