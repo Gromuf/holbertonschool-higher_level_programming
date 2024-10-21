1e18a45b8fdbd8522d919e428a5086646512e4a3
Gromuf <luigi.beaumois@gmail.com>
2024-10-21 13:46:20 +0200

    task 4

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,4 +1,4 @@
---create a table called first_table if it doesn't already exist
+-- create a table called first_table if it doesn't already exist
 CREATE TABLE IF NOT EXISTS first_table (
 	id INT,
 	name varchar(256)
