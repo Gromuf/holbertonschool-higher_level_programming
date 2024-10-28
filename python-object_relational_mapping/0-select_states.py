9bfdfe6afcab3962bdf1bf7c4243eb2419a8e317
Gromuf <luigi.beaumois@gmail.com>
2024-10-28 14:53:08 +0100

    azaaaaaa

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -20,10 +20,9 @@ if __name__ == "__main__":
                          passwd=argv[2],
                          db=argv[3])
     cursor = db.cursor()
-    cursor.execute(
-        "SELECT * FROM states ORDER BY id ASC"
-        )
-    for state in cursor.fetchall():
-        print(state)
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
     cursor.close()
     db.close()
