3d3850986ad53faf3b957c66352e4ef4d1b82687
Gromuf <luigi.beaumois@gmail.com>
2024-10-28 10:19:40 +0100

    correcting task 14

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,7 +1,7 @@
 -- 14-my_genres.sql
-SELECT genres.name
-FROM genres
-JOIN tv_show_genres ON genres.id = tv_show_genres.genre_id
+SELECT tv_genres.name
+FROM tv_genres
+JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
 WHERE tv_shows.title = 'Dexter'
-ORDER BY gernes.name ASC;
+ORDER BY gernes.name;
