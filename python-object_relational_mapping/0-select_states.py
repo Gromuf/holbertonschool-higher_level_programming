f98ef698f307b500a3a15fdc30088c29f17389fa
Gromuf <luigi.beaumois@gmail.com>
2024-10-28 13:43:47 +0100

    test task 0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -14,10 +14,14 @@ if __name__ == "__main__":
     Executes a query to fetch all states from the database and prints each row.
     """
 
-    db = MySQLdb.connect(user=argv[1],
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=argv[1],
                          passwd=argv[2],
                          db=argv[3])
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM `states`")
+    cursor.execute("SELECT * FROM `states` ORDER BY `id` ASC")
     for state in cursor.fetchall():
         print(state)
+    cursor.close()
+    db.close()
