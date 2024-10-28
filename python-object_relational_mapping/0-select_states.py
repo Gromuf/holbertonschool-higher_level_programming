3eac4859b25a76ebd67592ca9873a61122bb1190
Gromuf <luigi.beaumois@gmail.com>
2024-10-28 10:57:16 +0100

    pycodestyle task 0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -6,7 +6,7 @@ A script that lists all states from the hbtn_0e_0_usa database.
 
 This script connects to a MySQL database and retrieves all records
 from the states table, sorted by id in ascending order. The database
-credentials (username, password, and database name) are provided as 
+credentials (username, password, and database name) are provided as
 command line arguments.
 
 Usage:
@@ -15,9 +15,9 @@ Usage:
 
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost",
-                         user = argv[1],
-                         passwd = argv[2],
-                         db = argv[3])
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states ORDER BY id ASC;")
     rows = cursor.fetchall()
