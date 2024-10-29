b109647255b7af30a5b995d4cd15faab5ef42896
Gromuf <luigi.beaumois@gmail.com>
2024-10-29 09:38:47 +0100

    task 0 add port

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -16,6 +16,7 @@ if __name__ == "__main__":
     """
 
     db = MySQLdb.connect(host="localhost",
+                         port=3305,
                          user=argv[1],
                          passwd=argv[2],
                          db=argv[3])
