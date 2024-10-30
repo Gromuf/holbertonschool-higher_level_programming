e9491469c2d6cbbc9b37b975edac762b41341501
Gromuf <luigi.beaumois@gmail.com>
2024-10-30 10:46:54 +0100

    task 6 (po compris)

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+This module defines the State class that maps to the MySQL table `states`.
+"""
+
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class State(Base):
+    """
+    State class:
+    - Inherits from SQLAlchemy Base.
+    - Maps to the MySQL table `states`.
+    """
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+
+    name = Column(String(128), nullable=False)
