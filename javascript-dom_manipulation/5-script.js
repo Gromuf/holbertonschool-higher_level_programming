157470ddd506410759ef48e90dd6b3d86418a394
Gromuf <luigi.beaumois@gmail.com>
2024-11-22 10:43:12 +0100

    task 5

---
+++ javascript-dom_manipulation/5-script.js
@@ -0,0 +1,5 @@
+const button = document.querySelector("#update_header");
+const header = document.querySelector("header");
+button.addEventListener("click", () => {
+  header.textContent = "New Header!!!";
+});
